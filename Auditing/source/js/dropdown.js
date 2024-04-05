5473c4cb49230b451643e8f853c66843d8c2783a
P-JAGADISH <113572661+P-JAGADISH@users.noreply.github.com>
2024-04-05 06:13:38 -0700

    .

---
+++ Auditing/source/js/dropdown.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === "block") {
+      dropdown.style.display = "none";
+    } else {
+      dropdown.style.display = "block";
+    }
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.style.display === "block") {
+          openDropdown.style.display = "none";
+        }
+      }
+    }
+  }
+  
